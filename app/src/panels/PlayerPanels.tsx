81e1e6cdebb681f72d3e984ba70f142d0b5e9639
Patrick Beaume <p.beaume@gmail.com>
2024-12-12 09:54:21 +0100

    fix player panels

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { DistrictNoirRules } from '@gamepark/district-noir/DistrictNoirRules'
 import { StyledPlayerPanel, usePlayerId, usePlayers, useRules } from '@gamepark/react-game'
 import { FC } from 'react'
 import { createPortal } from 'react-dom'
@@ -7,9 +8,9 @@ import Player1 from '../images/player-1.jpg'
 import Player2 from '../images/player-2.jpg'
 
 export const PlayerPanels: FC<any> = () => {
-  const players = usePlayers()
-  const player = usePlayerId()
-  const rules = useRules()!
+  const players = usePlayers({ sortFromMe: true })
+  const rules = useRules<DistrictNoirRules>()!
+  const playerId = usePlayerId()
   const root = document.getElementById('root')
   if (!root) {
     return null
@@ -18,12 +19,12 @@ export const PlayerPanels: FC<any> = () => {
   return createPortal(
     <>
       {players.map((p) => {
-        const top = p.id === (player ?? rules.game.players[0])
+        const bottom = (p.id === (playerId ?? rules.players[0]))
         return (
           <StyledPlayerPanel
             key={p.id}
-            player={p.id}
-            css={top? topPanel: bottomPanel}
+            player={p}
+            css={bottom? bottomPanel: topPanel}
             backgroundImage={p.id === 1? Player1: Player2}
           />
         )
